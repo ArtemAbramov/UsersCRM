26a7d5a2fff1015dab826e99beaf2d000fb9930f
Artem Abramov <tanukisuicide@yandex.ru>
2019-05-26 12:09:58 +0300

    Added pagination

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -5,9 +5,9 @@
         <table>
           <thead>
             <tr>
-              <th @click='sort("name")'>Name</th>
-              <th @click='sort("age")'>Age</th>
-              <th @click='sort("gender")'>Gender</th>
+              <th @click='sort("name")'>Name &#8595;</th>
+              <th @click='sort("age")'>Age &#8595;</th>
+              <th @click='sort("gender")'>Gender &#8595;</th>
             </tr>
           </thead>
 
@@ -22,7 +22,20 @@
             </tr>
           </tbody>
         </table>
-        <p> debug: sort: {{ currentSort }}, dir: {{ currentSortDir }} </p>
+        <p style='text-align: center;'>
+          <span>debug: sort: {{ currentSort }}, dir: {{ currentSortDir }}</span>
+          <span> page: {{ this.page.current }}, length: {{ this.page.length }} </span>
+        </p>
+      </div>
+    </section>
+
+    <!-- Buttons -->
+    <section>
+      <div class="container">
+        <div class="button-list">
+          <div class="btn btnPrimary" @click='prevPage'> &#8592; </div>
+          <div class="btn btnPrimary" @click='nextPage'> &#8594; </div>
+        </div>
       </div>
     </section>
   </div>
@@ -36,7 +49,11 @@ export default {
     return {
       users: [],
       currentSort: 'name',
-      currentSortDir: 'asc'
+      currentSortDir: 'asc',
+      page: {
+        current: 1,
+        length: 3
+      }
     }
   },
   created () {
@@ -57,6 +74,10 @@ export default {
         if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
         if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
         return 0
+      }).filter((row, index) => {
+        let start = (this.page.current - 1) * this.page.length
+        let end = this.page.current * this.page.length
+        if (index >= start && index < end) return true
       })
     }
   },
@@ -66,6 +87,14 @@ export default {
         this.currentSortDir = this.currentSortDir  === 'asc' ? 'desc' : 'asc'
       }
       this.currentSort = e
+    },
+
+    // pagination
+    prevPage () {
+      if (this.page.current > 1) this.page.current -= 1
+    },
+    nextPage () {
+      if ((this.page.current * this.page.length) < this.users.length) this.page.current += 1      
     }
   }
 }
@@ -77,5 +106,15 @@ img {
   height: auto;
   border-radius: 50%;
   margin-right: 16px;
+}
+
+.button-list {
+  width: 100%;
+  text-align: center;
+
+  .btn {
+    border-radius: 60px;
+    margin: 0 20px;
+  } 
 }
 </style>
